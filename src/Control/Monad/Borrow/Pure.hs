17ce77709366963ffc8a90d20a5721dd966b48e6
Hiromi ISHII <konn.jinro@gmail.com>
2025-06-19 21:47:25 +0900

    execBO

--- src/Control/Monad/Borrow/Pure.hs
+++ src/Control/Monad/Borrow/Pure.hs
@@ -1,3 +1,12 @@
-module Control.Monad.Borrow.Pure (BO ()) where
+module Control.Monad.Borrow.Pure (
+  BO (),
+  execBO,
+
+  -- * Re-exports
+  module Control.Monad.Borrow.Pure.Lifetime,
+  module Control.Monad.Borrow.Pure.Lifetime.Token,
+) where
 
 import Control.Monad.Borrow.Pure.Internal
+import Control.Monad.Borrow.Pure.Lifetime
+import Control.Monad.Borrow.Pure.Lifetime.Token

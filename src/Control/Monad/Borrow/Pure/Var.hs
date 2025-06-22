0681b136550d630dffbc48c1bb99e112615d15b2
Hiromi ISHII <konn.jinro@gmail.com>
2025-06-22 14:05:05 +0900

    update and swap

--- src/Control/Monad/Borrow/Pure/Var.hs
+++ src/Control/Monad/Borrow/Pure/Var.hs
@@ -7,20 +7,27 @@
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
 {-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
-module Control.Monad.Borrow.Pure.Var (Var (), updateMutVar) where
+module Control.Monad.Borrow.Pure.Var (Var (), updateMutVar, swapMutVar) where
 
 import Control.Functor.Linear qualified as Control
 import Control.Monad.Borrow.Pure.Internal
 import Control.Monad.Borrow.Pure.Lifetime
+import Control.Syntax.DataFlow qualified as DataFlow
 import Data.Var.Linear (Var)
 import Data.Var.Linear qualified as MutVar
 
 updateMutVar :: (β <= α) => Mut α (Var a) %1 -> (a %1 -> BO β (a, b)) %1 -> BO β (Mut α (Var a), b)
-{-# NOINLINE updateMutVar #-}
-updateMutVar (UnsafeMut mv) f = Control.do
+{-# INLINE updateMutVar #-}
+updateMutVar (UnsafeMut mv) f = DataFlow.do
   -- NOTE: as there is only one reference to @'Var' a@, we can just use read/write
   -- instead of 'MutVar.atomicModify' (which requires pure function) while retaining atomicity.
-  (!a, mv) <- Control.pure (MutVar.readVar mv)
-  (!a, !b) <- f a
-  !mv <- Control.pure (MutVar.writeVar mv a)
-  Control.pure (UnsafeMut mv, b)
+  (a, mv) <- MutVar.readVar mv
+  f a Control.<&> \(!a, !b) -> DataFlow.do
+    mv <- MutVar.writeVar mv a
+    (UnsafeMut mv, b)
+
+swapMutVar :: (β <= α) => Mut α (Var a) %1 -> Mut α (Var a) %1 -> BO β (Mut α (Var a), Mut α (Var a))
+{-# INLINE swapMutVar #-}
+swapMutVar ma ma' = updateMutVar ma \a -> Control.do
+  (ma', a') <- updateMutVar ma' \a' -> Control.pure (a, a')
+  Control.pure (a', ma')

80fcedb1cddfab33feec76a9c2451bba983e8195
Hiromi ISHII <konn.jinro@gmail.com>
2026-03-23 21:32:52 +0900

    Tiny optimizations

--- src/Control/Concurrent/DivideConquer/Utils/QueuePool.hs
+++ src/Control/Concurrent/DivideConquer/Utils/QueuePool.hs
@@ -32,6 +32,7 @@ import Control.Concurrent (threadDelay)
 import Control.Concurrent.STM (atomically)
 import Control.Concurrent.STM.TMDeque (TMDeque, closeTMDeque, isClosedTMDeque, newTMDequeIO, pushFrontTMDeque, tryPopBackTMDeque, tryPopFrontTMDeque)
 import Control.Monad qualified as NonLinear
+import Control.Monad qualified as P
 import Control.Monad.Borrow.Pure
 import Control.Monad.Borrow.Pure.Internal
 import Data.Coerce (coerce)
@@ -113,7 +114,7 @@ popWork :: Mut α (QueuePool a) %1 -> BO α (Maybe (a, Mut α (QueuePool a)))
 popWork = Unsafe.toLinear \qs@(UnsafeAlias QueuePool {..}) ->
   unsafeSystemIOToBO do
     atomically (tryPopFrontTMDeque mine) P.>>= \case
-      Just mx -> P.pure $ (,qs) P.<$> mx
+      Just mx -> P.pure $ (,qs) P.<$!> mx
       Nothing ->
         let !n = V.length others
          in fix \go -> do
@@ -128,12 +129,12 @@ popWork = Unsafe.toLinear \qs@(UnsafeAlias QueuePool {..}) ->
                   g <- readIORef gen
                   let (!i, !g') = R.randomR (0, n P.- 1) g
                   writeIORef gen g'
-                  atomically (tryPopBackTMDeque (others V.! i)) P.>>= \case
+                  atomically (tryPopBackTMDeque (V.unsafeIndex others i)) P.>>= \case
                     Just Nothing -> P.pure Nothing
-                    Just mx -> P.pure $ (,qs) P.<$> mx
+                    Just mx -> P.pure $ (,qs) P.<$!> mx
                     Nothing -> do
                       g <- readIORef gen
-                      let (!i, !g') = R.randomR (10, 1_000) g
+                      let (!i, !g') = R.randomR (10, 100) g
                       writeIORef gen g'
                       threadDelay i
                       go
